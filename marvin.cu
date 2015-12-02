8c46ba186559b541dd22d40b1ed412abe45a78ce
Daniel Suo <dsuo@post.harvard.edu>
2015-12-02 00:05:14 -0500

    Update marvin.cu

--- marvin.cu
+++ marvin.cu
@@ -10,11 +10,11 @@ int main(int argc, char **argv){
     if (argc < 3 || argc >10){
         cout<<"Usage:"<<endl;
         cout<<argv[0]<<" train network.json [model1.marvin[,model2.marvin,...]] [snapshot_iteration]"<<endl;
-        cout<<"       example: "<<argv[0]<<" train mnist/lenet.json"<<endl;
+        cout<<"       example: "<<argv[0]<<" train examples/mnist/lenet.json"<<endl;
         cout<<argv[0]<<" test network.json model1.marvin[,model2.marvin,...] response_name1[,name2,...] file_name1.tensor[,name2.tensor,...] [save_every_n_iterations]"<<endl;
-        cout<<"       example: "<<argv[0]<<" test mnist/lenet.json ip1,conv2 mnist/ip1.tensor,mnist/conv2.tensor"<<endl;
+        cout<<"       example: "<<argv[0]<<" test examples/mnist/lenet.json examples/mnist/lenet.marvin ip1,conv2 examples/mnist/ip1.tensor,examples/mnist/conv2.tensor"<<endl;
         cout<<argv[0]<<" activate network.json model1.marvin[,model2.marvin,...] response_name_data response_name1[,name2,...] response1_channels[,response2_channels,...] file_prefix topK maxIterations"<<endl;
-        cout<<"       example: "<<argv[0]<<" activate mnist/lenet.json mnist/lenet.marvin data conv1,conv2 [0,1,2],[0,1,2,3,4,5] mnist/filters_ 100 20"<<endl;
+        cout<<"       example: "<<argv[0]<<" activate examples/mnist/lenet.json examples/mnist/lenet.marvin data conv1,conv2 [0,1,2],[0,1,2,3,4,5] examples/mnist/filters_ 100 20"<<endl;
         return 0;
 
     }
