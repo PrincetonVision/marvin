438a34d14e3adac343eba6840446832400cdb178
Fisher Yu <fy@cs.princeton.edu>
2016-02-27 22:32:44 -0500

    update compile.sh and CMakeLists.txt to cudnn 4.0

--- compile.sh
+++ compile.sh
@@ -4,10 +4,10 @@ export PATH=$PATH:/usr/local/cuda/bin
 
 if uname | grep -q Darwin; then
   CUDA_LIB_DIR=/usr/local/cuda/lib
-  CUDNN_LIB_DIR=/usr/local/cudnn/v4rc/lib
+  CUDNN_LIB_DIR=/usr/local/cudnn/v4/lib
 elif uname | grep -q Linux; then
   CUDA_LIB_DIR=/usr/local/cuda/lib64
-  CUDNN_LIB_DIR=/usr/local/cudnn/v4rc/lib64
+  CUDNN_LIB_DIR=/usr/local/cudnn/v4/lib64
 fi
 
-nvcc -std=c++11 -O3 -o marvin marvin.cu -I/usr/local/cuda/include -I/usr/local/cudnn/v4rc/include -L$CUDA_LIB_DIR -L$CUDNN_LIB_DIR -lcudart -lcublas -lcudnn -lcurand
+nvcc -std=c++11 -O3 -o marvin marvin.cu -I/usr/local/cuda/include -I/usr/local/cudnn/v4/include -L$CUDA_LIB_DIR -L$CUDNN_LIB_DIR -lcudart -lcublas -lcudnn -lcurand
